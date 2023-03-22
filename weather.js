a602a09d340ff87f945727c50d023176870773df
MaratGumerov <gumerovm74@gmail.com>
2023-03-23 00:27:23 +0300

    noted the skeleton of the arguments

--- weather.js
+++ weather.js
@@ -4,6 +4,19 @@ import { getArgs } from './helpers/args.js';
 const initCLI = () => {
     const args = getArgs(process.argv)
     console.log(args);
+    if (args.h) {
+        // Вывод Хелп
+    }
+
+    if (args.s) {
+        // Сохранить город
+    }
+
+    if (args.t) {
+        // сохранить токен
+    }
+
+    // Вывести погоду
 };
 
 initCLI();
