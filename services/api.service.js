068d66df3db571a52862ab05540998669d5f5ae2
MaratGumerov <gumerovm74@gmail.com>
2023-03-24 22:41:49 +0300

    added data results on display

--- services/api.service.js
+++ services/api.service.js
@@ -1,9 +1,31 @@
 import axios from 'axios';
 import { getKeyValue, TOKEN_DICTIONARY } from './storage.service.js';
 
+const getIcon = (icon) => {
+    switch(icon.slice(0, -1)){
+        case '01':
+            return '🌞';
+        case '02':
+            return '⛅';
+        case '03':
+            return '🌥';
+        case '04':
+            return '🌥';
+        case '09':
+            return '🌧';
+        case '10':
+            return '🌦';
+        case '11':
+            return '🌨';
+        case '13':
+            return '❆';
+        case '50':
+            return '🌊';
+    }
+};
+
 const getWeather = async (city) => {
     const token = await getKeyValue(TOKEN_DICTIONARY.token);
-    console.log(token);
     if (!token) {
         throw new Error('No api key.');
     }
@@ -19,4 +41,4 @@ const getWeather = async (city) => {
     return data;
 };
 
-export { getWeather };
+export { getWeather, getIcon };
